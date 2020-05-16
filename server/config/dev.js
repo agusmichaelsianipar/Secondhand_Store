e9475a6ee7d1c8bdada49525b33f4195320f1566
Jefri Manurung <jefrymanurung23@users.noreply.github.com>
2020-05-16 23:02:26 +0700

    change mongodb

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:'mongodb+srv://secondhand:VOqpNtR4R7d52UXJ@cluster0-on0xt.mongodb.net/test?retryWrites=true&w=majority'
+    // mongoURI:'mongodb+srv://secondhand:VOqpNtR4R7d52UXJ@cluster0-on0xt.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://manurung:manurung@cluster0-p6rjf.mongodb.net/test?retryWrites=true&w=majority'
 }
