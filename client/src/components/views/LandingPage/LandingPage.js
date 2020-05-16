73ae28142132f295c0dc4d85b8873316914169e0
M Anas Nasrulloh <AnasNasrull@users.noreply.github.com>
2020-05-16 16:46:21 +0700

    halaman detail produk dan fitur pencarian

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,129 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect,useState } from 'react'
+import Axios from 'axios';
+import { Icon, Col, Card,Row } from 'antd';
+import ImageSlider from '../../utils/ImageSlider';
+import SearchFeature from './Sections/SearchFeature';
+
+const {Meta} = Card;
+
 
 function LandingPage() {
+
+    const [Products,setProducts]=useState([])
+    const [Skip, setSkip] = useState(0)
+    const [Limit, setLimit] = useState(8)
+    const [PostSize, setPostSize] = useState(0)
+
+    const [SearchTerms, setSearchTerms] = useState("")
+
+    useEffect(()=>{
+        const variables = {
+            skip: Skip, 
+            limit: Limit
+        }
+        getProducts(variables)
+    },[])
+
+    const getProducts = (variables)=>{
+        Axios.post('/api/product/getProducts',variables)
+        .then(response => {
+            if (response.data.success) {
+                    if (variables.loadMore) {
+                        setProducts([...Products, ...response.data.products])
+                    } else {
+                        setProducts(response.data.products)
+                    }
+                    setPostSize(response.data.postSize)
+                } else {
+                    alert('Failed to fectch product datas')
+                }
+        })        
+    }
+
+    const onLoadMore = ( )=>{
+        let skip = Skip + Limit;
+        const variables = {
+            skip: skip, 
+            limit: Limit,
+            loadMore: true,
+            searchTerm: SearchTerms
+        }
+        getProducts(variables)
+
+        setSkip(skip)
+
+    }
+
+    const renderCards = Products.map((product, index)=>{
+
+        return <Col lg={6} md={8} xs={24}>
+                    <Card
+                        hoverable = {true}
+                        cover = {<a href={`/product/${product._id}`}><ImageSlider images={product.images}/></a>}
+                    >
+                        <Meta
+                            title = {product.title}
+                            description={`Rp${product.price}`}
+                        />
+                    </Card>
+                </Col>
+    })
+
+    const updateSearchTerms = (newSearchTerm) => {
+        
+        const variables = {
+            skip: 0,
+            limit: Limit,
+            searchTerm: newSearchTerm
+        }
+        setSkip(0)
+        setSearchTerms(newSearchTerm)
+
+        getProducts(variables)
+    }
+
     return (
         <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-        </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
+            <div style={{width:'75%', margin:'3rem auto'}}>
+                <div style={{textAlign:'center'}}>
+                    <h2>Let's Buy Any Secondhand's thing <Icon type="rocket" /></h2>
+                </div>
+                
+                {/*Filter*/}
+
+                {/*Search*/}
+                <div style={{ display:'flex', justifyContent:'flex-end', margin:'1rem auto'}}>
+                    
+                    <SearchFeature 
+                        refreshFunction={updateSearchTerms}
+                    />
+
+                </div>
+
+
+                {Products.length ===0 ?
+                <div style={{display:'flex', height:'300px', justifyContent:'center',alignItems:'center'}}>
+                    <h2>
+                        No Post Yet..
+                    </h2>
+                </div> :
+                <div>
+                    <Row gutter={[16,16]}>
+                        
+                        {renderCards}
+                    </Row>    
+                </div>
+            }
+            <br/><br/>
+
+            {PostSize >= Limit &&
+                <div style={{display:'flex', justifyContent:'center'}}>
+                    <button onClick={onLoadMore}>Load more ... </button>
+                </div>                      
+            }
+
+
+            </div>
         </>
     )
 }
