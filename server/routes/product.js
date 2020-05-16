58d8ae9392acb8687a679536ef450a50cc608387
Jefri Manurung <jefrymanurung23@users.noreply.github.com>
2020-05-16 23:03:07 +0700

    make limit show product

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { Product } = require("../models/Product");
+const {
+    Product
+} = require("../models/Product");
 
-const { auth } = require("../middleware/auth");
+const {
+    auth
+} = require("../middleware/auth");
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -21,7 +25,9 @@ var storage = multer.diskStorage({
     }
 })
 
-var upload = multer({ storage: storage }).single("file")
+var upload = multer({
+    storage: storage
+}).single("file")
 
 
 //=================================
@@ -32,9 +38,16 @@ router.post("/uploadImage", auth, (req, res) => {
 
     upload(req, res, err => {
         if (err) {
-            return res.json({ success: false, err })
+            return res.json({
+                success: false,
+                err
+            })
         }
-        return res.json({ success: true, image: res.req.file.path, fileName: res.req.file.filename })
+        return res.json({
+            success: true,
+            image: res.req.file.path,
+            fileName: res.req.file.filename
+        })
     })
 
 });
@@ -43,9 +56,34 @@ router.post("/uploadProduct", auth, (req, res) => {
 
     const product = new Product(req.body)
 
-    product.save((err)=> {
-        if(err) returnres.status(400).json({ success: false, err})
-        return res,status(200).json({ success: true})
+    product.save((err) => {
+        if (err) return res.status(400).json({
+            success: false,
+            err
+        })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+});
+
+router.post("/getProduct", auth, (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+
+    Product.find().populate("writer").sort([[sortBy, order]]).skip(skip).limit(limit).exec((err, products) => {
+        if (err) return res.status(400).json({
+            success: false,
+            err
+        })
+        return res.status(200).json({
+            success: true,
+            products,
+            postSize: products.length
+        })
     })
 
 });
